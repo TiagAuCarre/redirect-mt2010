fb3d566d448106e868f7868d7ee68f21f0f21e9e
Tiag's <128088506+TiagAuCarre@users.noreply.github.com>
2026-07-19 19:39:31 +0200

    Update

---
+++ redirects.js
@@ -0,0 +1,10 @@
+// Ajoute ou modifie tes raccourcis ici :
+// "ce-qu-il-y-a-apres-le-slash": "https://destination.com"
+//
+// Exemple : redirect.mondomaine.extension/instagram → Instagram
+const redirections = {
+  instagram: "https://instagram.com/toncompte",
+  youtube: "https://youtube.com/@toncompte",
+  site: "https://ton-site.com" ,
+  "lbc/all": "lbc/index.html"
+};
